d93dd520c6277cde62da157f3f958bb9268f14d2
Administrator <Administrator@OUZCAN>
2025-02-26 21:57:55 +0300

    Add project files.

---
+++ Presentation/ETicaretAPI.API/Controller/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using ETicaretAPI.Application.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        readonly private IProductWriteRepository _productWriteRepository;
+        readonly private IProductReadRepository _productReadRepository;
+
+        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+            _productReadRepository = productReadRepository;
+        }
+        [HttpGet]
+        public async void Get()
+        {
+            await _productWriteRepository.AddRangeAsync(new()
+            {
+                new()
+                {
+                    Id = Guid.NewGuid(), Name = "Product 1", Price = 100, CreateDate = DateTime.UtcNow, Stock=10
+                },
+                new()
+                {
+                    Id = Guid.NewGuid(), Name = "Product 2", Price = 200, CreateDate = DateTime.UtcNow, Stock=3
+                },
+                new()
+                {
+                    Id = Guid.NewGuid(), Name = "Product 3", Price = 300, CreateDate = DateTime.UtcNow, Stock=30
+                },
+            });
+            await _productWriteRepository.SaveAsync();
+        }
+    }
+}
