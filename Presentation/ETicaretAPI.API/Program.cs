4e5d902db6602ebeaf20e0488c15b8dd6ab0bd25
Ismail Oguzcan Aykar <oaykar53mac@gmail.com>
2025-03-12 06:37:49 +0300

    Dosya Yükleme Mekanizmasını Geliştirme

--- Presentation/ETicaretAPI.API/Program.cs
+++ Presentation/ETicaretAPI.API/Program.cs
@@ -25,6 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseStaticFiles();// wwwroot patghini kullanmak için
 app.UseCors();//CORS politikasýný kullanmak için
 
 app.UseHttpsRedirection();
