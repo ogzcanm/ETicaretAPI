b75df01a12abdc131a483037bb2f5db1693930a7
Ismail Oguzcan Aykar <oaykar53mac@gmail.com>
2025-03-27 09:27:26 +0300

    Kullanıcı Girişi(Authentication) İşlemleri

---
+++ Core/ETicaretAPI.Application/Exeptions/NotFoundUserExeption.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Exeptions
+{
+    public class NotFoundUserExeption : Exception
+    {
+        public NotFoundUserExeption() : base("Kullanıcı Adı veya Şifre Hatalı..!")
+        {
+        }
+
+        public NotFoundUserExeption(string? message) : base(message)
+        {
+        }
+
+        public NotFoundUserExeption(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
